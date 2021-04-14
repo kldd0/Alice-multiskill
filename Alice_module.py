56b68dfb471bd211997bfab50fe96df6f9eed9fe
kldd0 <steparesh@gmail.com>
2021-04-14 16:06:27 +0300

    base pattern

---
+++ Alice_module.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource
+from flask import request
+
+
+class Alice(Resource):
+    def post(self):
+        pass
+
+    def handle_dialog(self):
+        pass
---
+++ main.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+from flask import Flask
+from flask_restful import Api
+from Alice_module import *
+import logging
+import json
+import sys
+from abc import ABC, abstractmethod
+
+# базовое логирование (надо улучшить)
+logging.basicConfig(
+    filename='example.log',
+    format='%(asctime)s %(levelname)s %(name)s %(message)s'
+)
+
+app = Flask(__name__)
+api = Api(app)
+
+# secret key(надо настроить env)
+# app.config['SECRET_KEY']
+
+
+class Context:
+    _state = None
+
+    def __init__(self, state: State) -> None:
+        self.transition_to(state)
+
+    def transition_to(self, state: State):
+        self._state = state
+        self._state.context = self
+
+    def make_response(self, data):
+        self._state.make_response(data)
+
+    def handle_answer(self, data):
+        self._state.handle_answer(data)
+
+
+class State(ABC):
+
+    @property
+    def context(self) -> Context:
+        return self._context
+
+    @context.setter
+    def context(self, context: Context) -> None:
+        self._context = context
+
+    @abstractmethod
+    def make_response(self, data) -> None:
+        pass
+
+    @abstractmethod
+    def handle_answer(self, data) -> None:
+        pass
+
+
+def main():
+    # работа с Алисой
+    # тут будет происходить смена
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
