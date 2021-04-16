485a19225e507e7d66855aa3920b3844c901e025
kldd0 <steparesh@gmail.com>
2021-04-16 22:23:18 +0300

    ScanState #1

--- Context_module.py
+++ Context_module.py
@@ -1,22 +1,41 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
+from Alice_module import *
+import logging
+from dotenv import load_dotenv
+import requests
+import os
+
+env_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(env_path)
+logging.basicConfig(
+    filename='logs.log',
+    format='%(asctime)s %(levelname)s %(name)s %(message)s',
+    level=logging.DEBUG
+)
+
+SCAN_WORDS = {'проверь', 'просканируй', 'сканируй', 'чекни'}
+EXIT_WORDS = {'выход', 'пока', 'выйти', 'уйти', 'покинуть'}
+CHOICE_WORDS = {'функция', 'функции', 'возможности', 'возможность', 'варианты', 'вариант',
+                'модули', 'модуль'}
+
+API_KEY = os.getenv('API_KEY')
+VT_URL = 'https://www.virustotal.com/api/v3/urls'
 
 
 class Context:
     _state = None
 
-    def __init__(self, state: State) -> None:
+    def __init__(self, state):
         self.transition_to(state)
 
-    def transition_to(self, state: State):
+    def transition_to(self, state):
+        logging.info(f'Context: переключаемся в {type(state).__name__}')
         self._state = state
         self._state.context = self
 
-    def make_response(self, data):
-        self._state.make_response(data)
-
-    def handle_answer(self, data):
-        self._state.handle_answer(data)
+    def handle_dialog(self, res: AliceResponse, req: AliceRequest):
+        self._state.handle_dialog(res, req)
 
 
 class State(ABC):
@@ -30,9 +49,83 @@ class State(ABC):
         self._context = context
 
     @abstractmethod
-    def make_response(self, data) -> None:
+    def handle_dialog(self, res: AliceResponse, req: AliceRequest):
         pass
 
-    @abstractmethod
-    def handle_answer(self, data) -> None:
-        pass
+
+class ScanUrlState(State):
+    """
+    Реализация класса сканирования для удобной работы с VirusTotal API
+    """
+
+    def handle_dialog(self, res: AliceResponse, req: AliceRequest) -> None:
+        if set(req.words).intersection(SCAN_WORDS):
+            cleaned_request = self.__delete_unnecessary_words(req.words)
+            if type(cleaned_request) == str:
+                res.set_answer(self.scan(cleaned_request))
+            else:
+                res.set_answer('Вы не ввели ссылку, что мне проверять?')
+                self.context.transition_to(HelloState)
+
+        if set(req.words).intersection(EXIT_WORDS):
+            self.context.transition_to(HelloState())
+
+    @staticmethod
+    def __delete_unnecessary_words(words: list) -> str or dict:
+        for e in words:
+            if 'http' in e:
+                return e
+        return {'result': 'Not found'}
+
+    @staticmethod
+    def __get_url_id(url: str) -> str or json:
+        params = {'x-apikey': API_KEY}
+        req = requests.post(VT_URL, headers=params, data=f'url={url}')
+        if req.status_code == 200:
+            url_id = req.json()['data']['id']
+            return url_id
+        return {'status': 'error'}
+
+    @staticmethod
+    def __get_info(url_id: str) -> dict or str:
+        params = {'x-apikey': API_KEY}
+        response = requests.get(f'https://www.virustotal.com/api/v3/analyses/{url_id}', headers=params)
+        if response.status_code == 200:
+            res = dict(response.json()['data']['attributes']['results'])
+            results = dict()
+            for e in res.keys():
+                if res[e]['result'] in results.keys():
+                    results[res[e]['result']] += 1
+                else:
+                    results[res[e]['result']] = 1
+            return results
+        return {'status': 'error'}
+
+    def scan(self, url: str) -> str:
+        url_id = self.__get_url_id(url)
+        if type(url) == str:
+            info = self.__get_info(url_id)
+            if type(info) == dict:
+                return f'''Отчет антивирусов: {' '.join([f"{e} = {info[e]}" for e in info.keys()])}'''
+
+
+class HelloState(State):
+    def handle_dialog(self, res: AliceResponse, req: AliceRequest):
+        res.set_answer('Привет. Мы сделали прикольный навык!')
+        # self.context.transition_to(ChoiceState())
+        self.context.transition_to(ExitState())
+
+
+# class ChoiceState(State):
+#     def handle_dialog(self, res: AliceResponse, req: AliceRequest):
+#         res.set_answer('У нас есть несколько функций: переводчик и сканнер')
+
+
+class ExitState(State):
+
+    def handle_dialog(self, res: AliceResponse, req: AliceRequest):
+        res.set_answer('Привет. Работа с навыком закончена :)')
+        self.context.transition_to(HelloState())
+
+
+cnt = Context(ScanUrlState())
--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 import logging
-import json
-import sys
-import os
+from Context_module import cnt
+from Alice_module import *
 
 # базовое логирование (надо улучшить)
 logging.basicConfig(
     filename='logs.log',
-    format='%(asctime)s %(levelname)s %(name)s %(message)s'
+    format='%(asctime)s %(levelname)s %(name)s %(message)s',
+    level=logging.DEBUG
 )
 
 app = Flask(__name__)
 
 
+@app.route('/post', methods=['POST'])
 def main():
-    # работа с Алисой
-    # тут будет происходить смена состояний
-    app.run()
+    logging.info(f'Req: {request.json}')
+
+    alice_req = AliceRequest(request.json)
+    alice_resp = AliceResponse(alice_req)
+
+    cnt.handle_dialog(alice_resp, alice_req)
+    logging.info(f'Resp: {alice_resp}')
+    return alice_resp.to_json()
 
 
 if __name__ == "__main__":
-    main()
+    app.run(port=8989)
